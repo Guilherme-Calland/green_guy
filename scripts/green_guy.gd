3721f963991454520ccfc8351948c3d453aeaeec
Guilherme <gui.crb0@gmail.com>
2023-05-18 13:13:39 -0300

    moving

--- scripts/green_guy.gd
+++ scripts/green_guy.gd
@@ -1,10 +1,10 @@
-extends KinematicBody2D
+extends Node2D
 
 export var speed = 100
 var motion = Vector2(0,0)
 
-onready var animationPlayer = $Animation/AnimationPlayer
-onready var sprite = $Animation/Sprite
+onready var animationPlayer = $Body/Animation/AnimationPlayer
+onready var sprite = $Body/Animation/Sprite
 
 func run():
 	if Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
@@ -19,6 +19,7 @@ func run():
 	if motion.x != 0:
 		animationPlayer.playback_speed = 1.5
 	else:
-		animationPlayer.playback_speed = 1.0
+		animationPlayer.playback_speed = 1
 	
-	move_and_slide(motion, Vector2(0,-1))
+	$Body.move_and_slide(motion, Vector2(0,-1))
+	$Shadow.global_position.x = $Body.global_position.x
